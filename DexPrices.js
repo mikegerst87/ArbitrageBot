32facb6b56d9adf7dc5f3a4ed124ceab329892c1
mikegerst87 <mike.gerst87@gmail.com>
2022-01-06 09:24:13 -0700

    start project repo

--- DexPrices.js
+++ DexPrices.js
@@ -1,26 +1,33 @@
+
+const fs = require('fs');
 const { parse } = require('dotenv');
 const ethers = require('ethers');
-const { hexValue, isHexString } = require('ethers/lib/utils');
 const dotenv = require('dotenv').config();
 const traderJoeABI = require('./TraderJoeABI.json')
+const pangolinABI = require('./node_modules/@pangolindex/exchange-contracts/artifacts/contracts/pangolin-periphery/PangolinRouter.sol/PangolinRouter.json');
+const { parseUnits, formatEther } = require('ethers/lib/utils');
+const tokenList = require('./JoeTokenList.json');
+const { captureRejections } = require('events');
+const pangolinTokensJSON = require('./PangolinTokens.json');
+
+const qiAddress = '0x8729438eb15e2c8b576fcc6aecda6a148776c0f5';
+
+const usdtAddress = '0xc7198437980c041c805A1EDcbA50c1Ce5db95118';
+const wavaxAddress = '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7';
+
+const avaxProvider = new ethers.providers.JsonRpcProvider(process.env.AVAX_RPC_URL);
+const signer = avaxProvider.getSigner();
+const traderJoeContract = new ethers.Contract(process.env.JOE_ROUTER_ADDRESS, traderJoeABI.abi, avaxProvider);
+const pangolinContract = new ethers.Contract(process.env.PANGOLIN_ROUTER_ADDRESS, pangolinABI.abi, avaxProvider);
 
 async function main(){
 
-    const avaxProvider = new ethers.providers.JsonRpcProvider(process.env.AVAX_RPC_URL);
-    const signer = avaxProvider.getSigner();
+    
 
-    const block = await avaxProvider.getBlockNumber();
-
-    //create contract objects for exchanges
-    const traderJoeContract = new ethers.Contract(process.env.JOE_ROUTER_ADDRESS, traderJoeABI.abi, avaxProvider);
-
-    console.log(block);
-
+    GetPricesForAllPangolinMatchedTokens();
     //query exchanges for min out of stable coin for a token
-    const tokenInAmount  = 1;
-    const tokenPath = [process.env.CROWN_TOKEN_ADDRESS,process.env.MIM_TOKEN_ADDRESS];
-
-    const price = await traderJoeContract.getAmountsOut(tokenInAmount, tokenPath);
+    
+    //FindAllTokensTradableOnPangolin();
 
     //calculate slippage 
 
@@ -28,8 +35,55 @@ async function main(){
 
     //?? call flash loan contract that would execute exhange and exectution of arbitrage
 
-    console.log(parseInt(price[1]['_hex'], 16));
+    console.log( formatEther(await avaxProvider.getGasPrice()));
+
+    
+
+    
 }
+
+async function GetPriceForToken(tokenIn, tokenOut, decimals){
+    const tokenInAmount  = '1000000000000000000';
+
+    const tokenPath = [tokenIn, tokenOut];
+
+    const joePrice = await traderJoeContract.getAmountsOut(tokenInAmount, tokenPath);
+    const pangolinPrice = await pangolinContract.getAmountsOut(tokenInAmount, tokenPath);
+    
+    const joePriceToNumber = ethers.utils.formatUnits(joePrice[1], decimals);
+    const pangolinPriceToNumber = ethers.utils.formatUnits(pangolinPrice[1], decimals);
+
+    console.log(`JOE PRICE: ${joePriceToNumber} PANGOLIN PRICE: ${pangolinPriceToNumber}`);
+
+    return {
+        joePriceToNumber,
+        pangolinPriceToNumber
+    }
+}
+
+async function GetPricesForAllPangolinMatchedTokens(){
+    const tokens =pangolinTokensJSON.tokens;
+    for(let i = 0; i < tokens.length; i++){
+        const tokenAddress = tokens[i]['address'];
+        const tokenDec = tokens[i]['decimals'];
+        try{
+            const prices = await GetPriceForToken(tokenAddress, process.env.MIM_TOKEN_ADDRESS, tokenDec);
+        console.log(IsArbitrageOpportunity(prices), tokens[i]['symbol']);
+        }catch(error){
+            console.error(error);
+        }
+        
+        
+    }
+
+}
+
+function IsArbitrageOpportunity(prices){
+    return (prices.joePriceToNumber != prices.pangolinPriceToNumber && (Math.max(prices.joePriceToNumber, prices.pangolinPriceToNumber)- Math.abs(prices.joePriceToNumber - prices.pangolinPriceToNumber))/100 > .01);
+}
+
+
+
 
 
 main();
--- GetTradablePairs.js
+++ GetTradablePairs.js
@@ -1,6 +1,8 @@
 const ethers = require('ethers');
 const joeFactoryAbi = require('./JoeFactory.json');
+const pangolinFactoryAbi = require('./node_modules/@pangolindex/exchange-contracts/artifacts/contracts/pangolin-core/PangolinFactory.sol/PangolinFactory.json')
 const dotenv = require('dotenv').config();
+const {getPriceForToken} = require('./DexPrices.js')
 
 
 async function getPairs(){
@@ -8,20 +10,37 @@ async function getPairs(){
 
 
     const joeFactory = new ethers.Contract(joeFactoryAbi.address, joeFactoryAbi.abi, avaxProvider);
-    console.log(joeFactory)
+    const pangolinFactory = new ethers.Contract(process.env.PANGOLIN_FACTORY_ADDRESS,pangolinFactoryAbi.abi, avaxProvider);
+   
+
+    async function FindAllTokensTradableOnPangolin(){
+        let pangolinTokens = {
+            tokens: [
+            ]
+        }
+        const tokens = tokenList.tokens;
+        for(let i = 0; i < tokens.length; i++){
+            const tokenAddress = tokens[i].address;
+            const tokenDec = tokens[i].decimals;
+            console.log(tokens[i].symbol)
+            try{
+                await getPriceForToken(tokenAddress, wavaxAddress, 18);
+                pangolinTokens.tokens.push(tokens[i]);
+            }
+            catch(error){
+                console.log(error);
+                
+            }
+            
+        }
     
-
-    const numberOfTradablePairs = await joeFactory.allPairsLength();
-
-
-    for(let i = 0; i < numberOfTradablePairs; i++){
-        const pair = await joeFactory.allPairs(i);
-        console.log(pair);
+        fs.writeFile('./PangolinTokens.json',JSON.stringify(pangolinTokens), err =>{});
+        
     }
 
     //const networkWorking = await avaxProvider.detectNetwork();
 
-    console.log(tradablePairs);
+    console.log(`JOE TOTAL PAIRS: ${numberOfTradablePairsJoe} PANGOLIN TOTAL PAIRS: ${numberOfTradablePairsPangolin}`);
     
 }
 
